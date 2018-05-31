d9fa24a1b7d69cb6c293c04d16968fa50cd45d44
Tim Kettenacker <timkettenacker@Tims-MacBook-Pro.local>
2018-05-31 18:44:45 +0200

    included functions to query and cleanse wikidata responses

--- linked_approach.R
+++ linked_approach.R
@@ -1,27 +1,44 @@
+# define packages
+require(SPARQL)
+require(stringr)
+
+# define functions
+get_wikidata_id <- function(item){
+  query <- paste0('SELECT ?item ?itemLabel WHERE { ?item ?itemLabel "', item, '"@en.}' )
+  returned_res <- SPARQL(endpoint,query)$results
+  item_numbers <- str_extract(returned_res$item,"\\(?[0-9]+\\)?")
+  item_id <- item_numbers[!is.na(item_numbers)]
+  return(item_id)
+}
+
+determine_parent_instances <- function(item_id){
+  query <- paste0('SELECT ?parent ?parentLabel WHERE { wd:Q', item_id,' wdt:P31 ?parent .
+  SERVICE wikibase:label { bd:serviceParam wikibase:language
+                  "[AUTO_LANGUAGE],en". }
+                  }')
+  returned_res <- SPARQL(endpoint,query)$results
+  return(returned_res)
+}
+
 # step 1: extract entities from text
 # to-do: revise previous code and adjust when applicable
 # ...
 
-# step 2: get rich metadata from wikidata
-# on SPARQL: https://www.wikidata.org/wiki/Wikidata:SPARQL_query_service/queries
-# also provides an excellent query editor for interactive testing
+item <- "MongoDB"
 
-# take the least 'spoiled' R package to SPARQL wikidata without a fancy wrapper
-library(SPARQL)
+# step 2: get rich metadata from wikidata via the SPARQL services from: https://www.wikidata.org/wiki/Wikidata:SPARQL_query_service/queries
+# (also provides an excellent query editor for interactive testing)
+# https://www.wikidata.org/wiki/Wikidata:SPARQL_tutorial
 
 endpoint <- "https://query.wikidata.org/sparql"
 
+# retrieve wikidata id for the item value
+item_id <- get_wikidata_id(item = item)
+
 # get parent objects by looking for all 'instance of' triplets ('instance_of' has Id P31)
-# to-do: this will display only the Ids of the parents, review how to combine/traverse information
-
-query <- 'SELECT ?s ?label ?parent_obj WHERE {
-  ?s ?label "MongoDB"@en .
-  ?s wdt:P31 ?parent_obj .
-}'
-
-qd <- SPARQL(endpoint,query)
-df <- qd$results
+# this will display Ids and labels of the parents the item is pointing to
+instance_of_id <- determine_parent_instances(item_id)
 
 # ...
 
-# step 3: build graph, check suitability 
+# step 3: check how one can traverse the information, maybe build a graph, check suitability 
