1485426c8e0ae7d80c4e60bd65e29c8b9ea259b5
Tim Kettenacker <timkettenacker@Tims-MacBook-Pro.local>
2018-06-01 20:01:52 +0200

    extended function to accept all kind of edge labels, results are collected in a data frame

--- linked_approach.R
+++ linked_approach.R
@@ -11,10 +11,10 @@ get_wikidata_id <- function(item){
   return(item_id)
 }
 
-determine_parent_instances <- function(item_id){
-  query <- paste0('SELECT ?parent ?parentLabel WHERE { wd:Q', item_id,' wdt:P31 ?parent .
+determine_connected_nodes <- function(item_id, edge_id){
+  query <- paste0('SELECT ?parentObj ?parentObjLabel WHERE { wd:', item_id,' wdt:', edge_id, ' ?parentObj .
   SERVICE wikibase:label { bd:serviceParam wikibase:language
-                  "[AUTO_LANGUAGE],en". }
+                  "[AUTO_LANGUAGE], en". }
                   }')
   returned_res <- SPARQL(endpoint,query)$results
   return(returned_res)
@@ -35,9 +35,19 @@ endpoint <- "https://query.wikidata.org/sparql"
 # retrieve wikidata id for the item value
 item_id <- get_wikidata_id(item = item)
 
-# get parent objects by looking for all 'instance of' triplets ('instance_of' has Id P31)
-# this will display Ids and labels of the parents the item is pointing to
-instance_of_id <- lapply(item_id, determine_parent_instances)
+# get parent objects by looking for relationship triples (i.e. 'instance_of' has Id P31)
+# and pass to function which adds the ids and labels of the nodes the item is pointing to
+edge_ids <- list(instance_of=c("P31"), subclass_of=c("P279"), part_of=c("P361"))
+parentObj_df <- data.frame()
+for(i in 1:length(edge_ids)){
+  tryCatch({
+    resultset <- determine_connected_nodes(item_id = item_id, edge_id = edge_ids[[i]])
+    resultset$Relationship <- names(edge_ids[i])
+    parentObj_df <- rbind(parentObj_df, resultset)
+    print(parentObj_df)},
+    error=function(e){cat("ERROR :",conditionMessage(e), "\n")}
+  )
+}
 
 # ...
 
